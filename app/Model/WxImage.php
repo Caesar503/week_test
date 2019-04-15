a6e13024165020d4b18a30ec49cff1b7882db29c
zhaokai <15201130543@163.com>
2019-04-15 13:30:12 +0000

    管理素材

---
+++ app/Model/WxImage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WxImage extends Model
+{
+   protected $table = 'wx_image';
+    public $timestamps = false;
+}
---
+++ app/Model/WxText.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WxText extends Model
+{
+    protected $table = 'wx_text';
+    public $timestamps = false;
+}
