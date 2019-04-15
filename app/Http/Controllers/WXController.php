58ce951d8b88e23930215267d655db842b1f56b8
zhaokai <15201130543@163.com>
2019-04-15 01:46:25 +0000

    post 接收

--- app/Http/Controllers/WXController.php
+++ app/Http/Controllers/WXController.php
@@ -9,4 +9,13 @@ class WXController extends Controller
    	public function get_vaild(){
    		echo $_GET['echostr'];
    	}
+   	public function post_vaild(){
+   		// print_r($_POST);
+   		$content = file_get_contents("php://input");
+   		$res = simplexml_load_string($content);
+   		// dd($res);/
+   		$str = $time.$content."\n";
+   		file_put_contents("logs/wx_event.log",$str,FILE_APPEND);
+   		
+   	}
 }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//第一次
 Route::get('Weixin/vaild','WXController@get_vaild');
+// 第二次以及以后
+Route::post('Weixin/vaild','WXController@post_vaild');
