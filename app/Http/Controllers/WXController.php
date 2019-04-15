f7f76cd6a92775189302aeb5a83d7e4ee5e74945
zhaokai <15201130543@163.com>
2019-04-15 01:10:35 +0000

    上线

---
+++ app/Http/Controllers/WXController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WXController extends Controller
+{
+   	public function get_vaild(){
+   		print_r($_GET);
+   	}
+}
